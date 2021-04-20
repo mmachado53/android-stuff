ff46459c4b167422fc9aad7e37d4cc8464dec482
Miguel Machado <mmachado53@gmail.com>
2021-04-19 20:02:34 -0600

    SimpleMVVMApp - Setting main actionbar to work with navigation components

--- SimpleMVVMApp/app/src/main/java/com/mmachado53/simplemvvmapp/ui/MainActivity.kt
+++ SimpleMVVMApp/app/src/main/java/com/mmachado53/simplemvvmapp/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.mmachado53.simplemvvmapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mmachado53.simplemvvmapp.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // setting actionbar to work with navigation components
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
